88e10842fc6f9a268d5b86c6e8aed039d87e80e4
Brian Button <brian.button@centurylinkcloud.com>
2014-05-16 11:23:39 -0500

    Initial commit for DotNetWorkerApp

---
+++ SimpleWorkerApp/DotNetWorkerApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DotNetWorkerApp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Here I am!!!");
+                Thread.Sleep(5000);
+            }
+        }
+    }
+}
