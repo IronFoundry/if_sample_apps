651cd6ec7c08d4900819ca4f15d73bbd63f78173
Brian Button <brian.button@centurylinkcloud.com>
2014-05-18 15:29:05 -0500

    Corrected namespace on HomeController

--- MsSQL-Binding-Sample/SoftballStatsViewer/Controllers/HomeController.cs
+++ MsSQL-Binding-Sample/SoftballStatsViewer/Controllers/HomeController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace SoftballStatsViewer.Models.Models.Controllers
+namespace SoftballStatsViewer.Controllers
 {
     public class HomeController : Controller
     {
